trpo-2020-sandbox/merge-conflict-easy-OldBoots

Count array items with macro

main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define N_ELEMENTS(arr) (sizeof(arr) / sizeof(*(arr)))
+
 int min_element(int* arr, int size)
 {
     int min = *arr;
@@ -18,11 +20,11 @@ int main()
 {
     int arr[] = {3, 1, 4, 1, 5, 9, 2};
 
-    const int min = min_element(arr, 7);
+    const int min = min_element(arr, N_ELEMENTS(arr));
 
     printf("Array: ");
     int i;
-    for (i = 0; i < 7; ++i) {
+    for (i = 0; i < N_ELEMENTS(arr); ++i) {
         printf("%d ", arr[i]);
     }
     printf("\n");

trpo-2020-sandbox/rebase-conflict-hard-z0rg1x

main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define N_ELEMENTS(arr) (sizeof(arr) / sizeof(*(arr)))
+
 int max_element(int* arr, int size)
 {
     int max = *arr;
@@ -18,11 +20,11 @@ int main()
 {
     int arr[] = {3, 1, 4, 1, 5};
 
-    const int max = max_element(arr, 5);
+    const int max = max_element(arr, N_ELEMENTS(arr));
 
     printf("Array: ");
     int i;
-    for (i = 0; i < 5; ++i) {
+    for (i = 0; i < N_ELEMENTS(arr); ++i) {
         printf("%d ", arr[i]);
     }
     printf("\n");